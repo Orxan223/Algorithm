9805e1524d26204d9365ba54d5ffee76a18bf225
Orxan223 <or.xan.2018@bk.ru>
2022-02-06 11:51:00 +0300

    UP

---
+++ Week02/Day02/Class/main.js
@@ -0,0 +1,117 @@
+// // const { Ask } = require('asfk');
+
+// // Ask([""]).then(([a]) => {
+// //     a = parseInt(a);
+// //     let ilkReqem = parseInt(a  / 1000);
+// //     let sonReqem = a % 10;
+// //     let cem = ilkReqem + sonReqem;
+// //     console.log(cem);
+// // });
+
+
+// const { Ask } = require('asfk');
+
+// Ask([""]).then(([a]) => {
+//     a = parseInt(a);
+//     let ilkReqem = parseInt(a  / 1000);
+//     let Son = a % 10;
+//     let cem = ilkReqem + Son;
+//     console.log(cem);
+
+
+// });
+
+
+
+// const { Ask } = require('asfk');
+
+// Ask([""]).then(([a]) => {
+//     a = parseInt(a);
+//     let ilkReqem = parseInt(a  / 1000);
+//     let sonReqem = a % 10;
+//     let cem = ilkReqem + sonReqem;
+//     console.log(cem);
+// });
+
+
+// const { Ask } = require('asfk');
+
+// Ask([""]).then(([a]) => {
+//     a = parseInt(a);
+//     let ilk = parseInt(a  / 100);
+//     let orta = parseInt(a  / 10) % 10 ;
+//     let son = parseInt(a  % 10);
+
+//     let cem = ilk + orta + son
+//     console.log(cem);
+
+
+
+// });
+
+
+// const { Ask } = require('asfk');
+
+// Ask([""]).then(([a]) => {
+//     a = parseInt(a);
+//     let eded = parseInt(a  / 100)%10;
+
+//     let cem = eded
+//     console.log(eded);
+
+
+
+// });
+
+
+// const { Ask } = require('asfk');
+
+// Ask([""]).then(([a]) => {
+//     a = parseInt(a);
+//     let ilk = parseInt(a  / 10);
+ 
+
+//     console.log(ilk);
+
+
+
+// });
+
+
+
+
+// const { Ask } = require('asfk');
+
+// Ask([""]).then(([a]) => {
+//     a = parseInt(a);
+//     let ilk = parseInt(a  / 100);
+//     let orta = parseInt(a  / 10) % 10 ;
+//     let son = parseInt(a  % 10);
+//     let cem = ilk * orta * son
+//     console.log(cem);
+
+// });
+
+
+// const { Ask } = require('asfk');
+
+// Ask([""]).then(([a]) => {
+//     a = parseInt(a);
+//     let ilk =  parseInt(a  / 100);
+//     let son = a  % 10;
+//     let cem = parseInt(ilk * 10) + son
+//     console.log(cem);
+// });
+
+
+const { Ask } = require('asfk');
+
+Ask(["",""]).then(([a,b]) => {
+    a = parseInt(a);
+    b = parseInt(b);
+    
+    let cem = a ** 2 + b **2
+    console.log(cem);
+});
+
+
