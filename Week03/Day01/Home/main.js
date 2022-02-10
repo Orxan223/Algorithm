497ac35ac410c9fccaa68561ea375e47cb87108e
Orxan223 <or.xan.2018@bk.ru>
2022-02-10 17:47:17 +0300

    Up

---
+++ Week03/Day01/Home/main.js
@@ -0,0 +1,94 @@
+// -----------1-----------
+// const { Ask } = require("asfk");
+
+// Ask(["", "", ""]).then(([a, x, c]) => {
+//   if (a <= x && x <= c) {
+//     console.log("yes");
+//   } else {
+//     console.log("NO");
+//   }
+// });
+
+// ------------2---------------
+// const { Ask } = require("asfk");
+
+// Ask([""]).then(([a]) => {
+//   if ((a % 2 == 0)) {
+//     console.log(a-1);
+//   } else if(((a % 2 == 1 )) ){
+//     console.log(a-2);
+//   }
+// });
+
+// ------------3---------------
+// const { Ask } = require("asfk");
+
+// Ask(["", ""]).then(([a, b]) => {
+//   a = parseInt(a);
+//   b = parseInt(b);
+//   if ((c = parseInt(a / b)) && (d = a % b)) {
+//     console.log(c,d);
+//   } else {
+//     console.log("Divisible");
+//   }
+// });
+
+// ---------4---------------
+// const { Ask } = require("asfk");
+
+// Ask([""]).then(([a]) => {
+//   if ((a % 2 == 0)) {
+//     console.log(a-2);
+//   } else if(((a % 2 == 1 )) ){
+//     console.log(a-1);
+//   }
+// });
+
+// --------------5--------------
+// const { Ask } = require("asfk");
+
+// Ask([""]).then(([a]) => {
+//   a = parseInt(a);
+//   if (a >= 3 && a <= 5) {
+//     console.log("Spring");
+//   } else if (a > 5 && a <= 8) {
+//     console.log("Summer");
+//   } else if (a > 8 && a <= 11) {
+//     console.log("Autumn ");
+//   } else if (a > 11 && a <= 14) {
+//     console.log("Winter");
+//   }
+// });
+
+// --------------6-----------
+// const { Ask } = require("asfk");
+
+// Ask([""]).then(([a]) => {
+//   if (a > 0) {
+//     console.log("Water");
+//   } else {
+//     console.log("Ice");
+//   }
+// });
+
+// ------------------7---------------
+// const { Ask } = require('asfk');
+
+// Ask([""]).then(([a]) => {
+//     a=parseInt(a)
+//     let r = parseInt(a/1000)
+//     let b = parseInt(a/100%10)
+//     let c = parseInt(a/10%10)
+//     let d= parseInt(a/10)
+//     if(r=d && b==c){
+//         console.log("yes");
+
+//     }
+//     else{
+//         console.log("no");
+//     }
+    
+// });
+
+
+
