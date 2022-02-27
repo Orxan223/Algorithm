9710b27920fb618f8c74f221ef307e3e65749303
Orxan223 <or.xan.2018@bk.ru>
2022-02-27 12:47:15 +0300

    Up

---
+++ Week05/Day01/Class/main.js
@@ -0,0 +1,55 @@
+// const { Ask } = require('asfk');
+
+// Ask(["", ""]).then(([n, arr]) => {
+//     n = parseInt(n);
+//     arr = arr.split(" ").map(Number);
+
+
+//     let cem = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         cem += arr[i]  
+        
+//     }
+//     console.log(cem);
+// });
+
+
+
+// const { Ask } = require('asfk');
+
+// Ask(["", ""]).then(([n, arr]) => {
+//     n = parseInt(n);
+//     arr = arr.split(" ").map(Number);
+
+
+//     let larg = arr[0]
+//     for (let i = 0; i < arr.length; i++) {
+//         if(larg<arr[i]){
+//             larg=arr[i]
+//         }  
+
+//     }
+//     console.log(larg);
+
+// })
+
+
+
+
+const { Ask } = require('asfk');
+
+Ask(["", ""]).then(([n, arr]) => {
+    n = parseInt(n);
+    arr = arr.split(' ').map(Number);
+    let arr1 = [];
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] >= 0) arr1.push(arr[i] + 2);
+        else arr1.push(arr[i]);
+    };
+    console.log(...arr1);
+});
+
+//task 4
+// boyuk - kicik = netice
+
+
