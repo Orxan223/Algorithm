42d2161b28a2e91d042e90873660666e1d6e26d3
Orxan223 <or.xan.2018@bk.ru>
2022-03-04 14:50:48 +0300

    UP

--- Week05/Day02/Class/main.js
+++ Week05/Day02/Class/main.js
@@ -1,91 +1,4 @@
 // const { Ask } = require('asfk');
-
-// Ask(["", ""]).then(([n, arr]) => {
-//     n = parseInt(n);
-//     arr = arr.split(" ").map(Number);
-
-//     let larg = arr[0];
-//     let min = arr[0];
-//     let cem = 0;
-//     let say = 0;
-//     let say1 = 0;
-//     for (let i = 0; i < n; i++) {
-//         if (arr[i]>larg) {
-//             larg = arr[i];
-//             say = 1;
-//         }else if (arr[i]==larg) {
-//             say++
-//         }
-//     }
-//     for (let i = 0; i < n; i++) {
-//         if (arr[i]<min) {
-//             min = arr[i];
-//             say1= 1;
-//         }else if (arr[i]==min) {
-//             say1++
-//         }
-//     }
-//     for (let i = 0; i < n; i++) {
-//         cem += arr[i];
-//     }
-
-//     console.log(cem-((say*larg)+(say1*min)));
-// });
-
-//task2
-// const { Ask } = require('asfk');
-
-// Ask(["", ""]).then(([n, arr]) => {
-//     n = parseInt(n);
-//     arr = arr.split(" ").map(Number);
-//     let index = 0;
-//     let larg = arr[0];
-//     for (let i = n-1; i > 0;i--) {
-//         if (arr[i]>larg) {
-//             larg = arr[i];
-//             index = i;
-//             arr[index] = arr[n-1];
-//             arr[n-1] = larg
-//         }
-//     }
-//     console.log(...arr);
-// });
-
-
-//     let temp = arr[index];
-//     arr[index] = arr[-1];
-//     arr[-1] = temp;
-
-//     console.log(larg);
-// })
-
-
-// const { Ask } = require('asfk');
-
-// Ask(["", ""]).then(([n, arr]) => {
-//     n = parseInt(n);
-//     arr = arr.split(" ").map(Number);
-//     let arr1 = [];
-
-//     let larg = arr[0]
-//     for (let i = arr.length -1 ; i>=0 ;  i--) {
-//         arr1.push(arr[i])
-
-
-//     }
-//     console.log(...arr1);
-
-// })
-
-
-
-
-
-
-
-
-
-// const { Ask } = require('asfk');
 // Ask(["", ""]).then(([n, arr]) => {
 //     n = parseInt(n)
 //     arr = arr.split(" ").map(Number)
@@ -109,8 +22,7 @@
 //     }
 //     console.log(sum);
 // });
-
-
+// --------------------TASK 2 --------------
 // const { Ask } = require('asfk');
 // Ask(["", ""]).then(([n, arr]) => {
 //     n = parseInt(n)
@@ -135,58 +47,65 @@
 
 
 
-//task4
+// --------------------TASK 3 ---------------------
+// const { Ask } = require('asfk');
+
+// Ask(["", ""]).then(([n, arr]) => {
+//     n = parseInt(n);
+//     arr = arr.split(" ").map(Number);
+//     let arr1 = [];
+
+//     let larg = arr[0]
+//     for (let i = arr.length -1 ; i>=0 ;  i--) {
+//         arr1.push(arr[i])
+
+
+//     }
+//     console.log(...arr1);
+
+// })
+
+
+
+
+
+
 // const { Ask } = require('asfk');
 // Ask(["", ""]).then(([arr1, arr2]) => {
 //     arr1 = arr1.split(" ").map(Number)
 //     arr2 = arr2.split(" ").map(Number)
-//     let sum = 0
-//     let say = arr1.length + arr2.length
-//     for (let i = 0; i < arr1.length; i++) {
-//         sum += arr1[i]
+//     let cem = 0 
+//     let sayi = arr1.length + arr2.length
+//     for (let index = 0; index < arr1.length; index++) {
+//         cem = cem + arr1[index];
+
+        
 //     }
-//     for (let i = 0; i < arr2.length; i++) {
-//         sum += arr2[i]
+//     for (let index = 0; index < arr2.length; index++) {
+//         cem = cem + arr2[index];
+        
 //     }
-//     console.log(sum / say);
+//     console.log(cem / sayi);
 // });
 
-//task5
+
+
 // const { Ask } = require('asfk');
 // Ask([""]).then(([arr]) => {
 //     arr = arr.split(" ").map(Number)
-//     let start = 0
+//     let baslangic = 0
 //     for (let i = 0; i < arr.length; i++) {
-//         if (arr.includes(start)) {
-//             start++
+//         if (arr.includes(baslangic)) {
+//             baslangic++
 //             continue
 //         }
 //         else
 //             break
 //     }
-//     console.log(start);
+//     console.log(baslangic);
 // });
 
 
 
-const { Ask } = require('asfk');
 
-Ask(["", ""]).then(([n, arr]) => {
-    n = parseInt(n);
-    arr = arr.split(" ").map(Number);
-    let missing = 1
-    let a = 0
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i]>0) {
-            
-        }
-        
-        if (arr[i]==missing&&arr[i]!=a) {
-            a=arr[i]
-            missing++;
-        }
 
-        
-    }
-console.log(missing);
-});
