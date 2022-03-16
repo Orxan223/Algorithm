1da2db48af3204b692329d4077951b378e63de7c
Orxan223 <or.xan.2018@bk.ru>
2022-03-16 14:50:15 +0300

    UP

--- Week06/Day02/Class/main.js
+++ Week06/Day02/Class/main.js
@@ -21,24 +21,51 @@
 //     console.log(cem,count);
 // });
 
-
-// TASK 2
 // const { Ask } = require('asfk');
-// Ask(["", "","",""]).then(([n, arr,c,r]) => {
+// Ask(["", "", "", ""]).then(([n, arr, a, b]) => {
 //     n = parseInt(n);
+//     a = parseInt(a)
+//     b = parseInt(b)
 //     arr = arr.split(" ").map(Number);
-//     let matrix = [];
-//     let cem = 0
-//     let count = 0
+//     let matrix = []
 //     for (let i = 0; i < n; i++) {
-//         matrix.push(arr.splice(0, n));
-//     };
-//     for (let i = 0; i < c; i++) {
-//         for (let j = 0; j < r; j++) {
-           
+//         matrix.push(arr.splice(0, n))
+//     }
+//     matrix.splice(a)
+//     for (let i = 0; i < a; i++) {
+//         matrix[i].splice(b)
+//     }
+//     console.log(matrix);
+// });
+
+
+
+
+
+// const { Ask } = require('asfk');
+// Ask(["", "", ""]).then(([n, a, arr]) => {
+//     n = parseInt(n);
+//     a = parseInt(a)
+//     arr = arr.split(" ").map(Number);
+//     let matrix = []
+//     for (let i = 0; i < n; i++) {
+//         matrix.push(arr.splice(0, a))
+//     }
+
+//     let min = matrix[0][0]
+
+//     for (let i = 0; i < n; i++) {
+//         for (let j = 0; j < a; j++) {
+//             if (matrix[i][j] < min) {
+//                 min = matrix[i][j]
+//             }
 //         }
 //     }
-//     console.log(cem,count);
+//     for (let i = 0; i < n; i++) {
+//         if (matrix[i].includes(min)) {
+//             console.log(i + 1);
+//         }
+//     }
 // });
 
 
@@ -48,3 +75,21 @@
 
 
 
+
+// const { Ask } = require('asfk');
+// Ask([""]).then(([n]) => {
+//     n = parseInt(n);
+//     let arr = []
+//     let matrix = []
+//     let a = 1
+//     let b = Math.ceil(n ** 2 / 2) + 1
+//     while (a < Math.ceil(n ** 2 / 2) + 1) {
+//         arr.push(a, b)
+//         a++
+//         b++
+//     }
+//     for (let i = 0; i < n; i++) {
+//         matrix.push(arr.splice(0, n))
+//     }
+//     console.log(matrix);
+// });
