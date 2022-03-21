d56321221fc5c2599fb96c5e385f4b27c7a3bd5a
Orxan223 <or.xan.2018@bk.ru>
2022-03-21 14:19:18 +0300

    UP

--- Week07/Day02/Class/main.js
+++ Week07/Day02/Class/main.js
@@ -1,24 +1,27 @@
-// const { Ask } = require("asfk");
-// Ask(["", ""]).then(([arr, n]) => {
-//     let count = 0;
-    // for (let i = 0; i < arr.length; i++) {
-    //     if (arr[i].toUpperCase() == n.toUpperCase()) {
-    //         count++;
-    //     }
-    // }
-//     console.log(count);
+// function say(string,char) {
+//     let count = 0
+//     for (let i = 0; i < string.length; i++) {
+//         if (string[i] == char || string[i] == char.toUpperCase()) {
+//             count++;
+//         }
+//     }
+//     return count
+// }
+
+// console.log(say('Hello world', 'l')); 
+
+
+
+
+const { Ask } = require('asfk');
+// Ask([""]).then(([a]) => {
+//     function say(a) {
+//         let eded = []
+//         for (let i = a.length - 1; i >= 0; i--) {
+//             eded.push(a[i])
+//         }
+//         return eded
+
+//     }
+//     console.log(say(a));
 // })
-
-
-function say(string,char) {
-    let count = 0
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] == char || string[i] == char.toUpperCase()) {
-            count++;
-        }
-    }
-    return count
-}
-
-console.log(say('Hello world', 'l')+1); 
-
