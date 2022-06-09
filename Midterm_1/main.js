673c2de597642deb1d3ee0f588760b82c95d4149
Orxan223 <or.xan.2018@bk.ru>
2022-06-09 15:14:57 +0300

    UP

--- Midterm_1/main.js
+++ Midterm_1/main.js
@@ -108,3 +108,125 @@
 
 
 
+
+
+
+
+
+
+
+// Task 1:
+// const { Ask } = require('asfk');
+// Ask([""]).then(([eded]) => {
+//     eded = parseInt(eded)
+//     let teklik = eded % 10;
+//     let onluq = parseInt(eded / 10);
+//     let cem = teklik + onluq;
+//     console.log(cem);
+// });
+
+
+
+
+
+
+// Task 2:
+// const { Ask } = require('asfk');
+// Ask([""]).then(([eded]) => {
+//     eded = parseInt(eded);
+//     let a = parseInt(eded / 100);
+//     let b = parseInt(eded / 10) % 10;
+//     let c = eded % 10;
+//     let abc = eded;
+//     let acb = a * 100 + c * 10 + b;
+//     let bac = b * 100 + a * 10 + c;
+//     let bca = b * 100 + c * 10 + a;
+//     let cab = c * 100 + a * 10 + b;
+//     let cba = c * 100 + b * 10 + a;
+//     if (abc > acb && abc > bac && abc > bca && abc > cab && abc > cba) console.log(abc**2);
+//     else if (acb > bac && acb > bca && acb > cab && acb > cba) console.log(acb**2);
+//     else if (bac > bca && bac > cab && bac > cba) console.log(bac**2);
+//     else if (bca > cab && bca > cba) console.log(bca**2);
+//     else if (cab > cba) console.log(cab**2);
+//     else console.log(cba**2);
+// });
+
+
+
+
+
+
+
+
+// Task 3:
+// Yalnız 5 rəqəmli ədəd üçün həll:
+
+// const { Ask } = require('asfk');
+// Ask([""]).then(([n]) => {
+//     a = parseInt(n / 10000);
+//     b = parseInt(n / 1000) % 10;
+//     c = parseInt(n / 100) % 10;
+//     d = parseInt(n / 10) % 10;
+//     e = n % 10;
+//     if (a == b || a == c || a == d || a == e || b == c || b == d || b == e || c == d || c == e || d == e) console.log("YES");
+//     else console.log("NO");
+// });
+// n rəqəmli ədəd üçün həll:
+
+// const { Ask } = require('asfk');
+// Ask([""]).then(([n]) => {
+//     n = parseInt(n);
+//     let result = false;
+//     while (n > 0) {
+//         let temp = parseInt(n / 10);
+//         let sonReqem = n % 10;
+//         while (temp > 0) {
+//             if (sonReqem == temp % 10) {
+//                 result = true;
+//                 break;
+//             };
+//             temp = parseInt(temp / 10);
+//         };
+//         if (result) break;
+//         n = parseInt(n / 10);
+//     };
+//     if (result) console.log('YES');
+//     else console.log('NO');
+// });
+
+
+
+
+
+
+
+// Task 4:
+// const { Ask } = require('asfk');
+// Ask([""]).then(([n]) => {
+//     for (let i = 1; i <= n; i++) {
+//         if (n % i == 0 && i % 2 == 1) {
+//             console.log(i);
+//         }
+//     }
+// });
+
+
+
+
+
+
+
+
+// Task 5:
+// const { Ask } = require('asfk');
+// Ask(["", ""]).then(([a, b]) => {
+//     a = parseInt(a);
+//     b = parseInt(b);
+//     let cem = 0;
+//     for (let i = a; i <= b; i++) {
+//         if (i % 2 != 0) {
+//             cem += i;
+//         };
+//     };
+//     console.log(cem);
+// });
